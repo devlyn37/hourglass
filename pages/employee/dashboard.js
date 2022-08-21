d600b473b1060c5b9a8c9ab9ffe3aa76a6fd8ddb
eduairet <hola@eduairet.com>
2022-08-21 01:58:35 -0500

    Styling done

--- pages/employee/dashboard.js
+++ pages/employee/dashboard.js
@@ -1,47 +1,54 @@
-import SmallLogo from "../../components/smallLogo.js"
-import Link from "next/Link"
-import React from "react";
-import styles from "../../styles/dashboard.module.css"
+import SmallLogo from '../../components/smallLogo.js';
+import Link from 'next/Link';
+import React from 'react';
+import styles from '../../styles/dashboard.module.css';
 
 export default class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            employeeName: "Satoshi Nakamoto",
-            employeeEmail: "satoshi@gmail.com",
-            streamBalance: "3,457.29",
-            currency: "USD", stakePercent: "5.0%", stakeRewards: "25.23", debitBalance: "2,945.50"
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      employeeName: 'Satoshi Nakamoto',
+      employeeEmail: 'satoshi@gmail.com',
+      streamBalance: '3,457.29',
+      currency: 'USD',
+      stakePercent: '5.0%',
+      stakeRewards: '25.23',
+      debitBalance: '2,945.50',
+    };
+  }
 
-    render() {
-        return (
-            <div className={styles.container}>
-
-                <div className={styles.navBar}>
-                    <SmallLogo />
-                    <p>{this.state.employeeName}</p>
-                </div>
-
-                <div className={styles.dashboard}>
-                    <p className={styles.streamBalance}>{this.state.streamBalance}</p>
-                    <div className={styles.dashboardDetails}>
-                        <div className={styles.dashboardTile}>
-                            <p>Staking rewards</p>
-                            <span>{this.state.stakePercent}</span>
-                            <span>{this.state.stakeRewards}</span>
-                        </div>
-                        <div className={styles.dashboardTile}>
-                            <p>Debit balance available</p>
-                            <span>{this.state.debitBalance}</span>
-                        </div>
-                    </div>
-                    <div>
-                        <button className={styles.button}>Send to wallet</button>
-                        <button className={styles.button}>Send to bank</button>
-                    </div>
-                </div>  
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.navBar}>
+          <SmallLogo />
+          <p className={styles.employee}>{this.state.employeeName}</p>
+        </div>
+        <div className={styles.dashboard}>
+          <p
+            className={styles.streamBalance}
+          >{`$${this.state.streamBalance}`}</p>
+          <div className={styles.dashboardDetails}>
+            <div className={styles.dashboardTile}>
+              <p>
+                <strong>Staking rewards</strong>
+              </p>
+              <span>{`${this.state.stakePercent} - `}</span>
+              <span>{`$${this.state.stakeRewards}`}</span>
             </div>
-        );
-    }
+            <div className={styles.dashboardTile}>
+              <p>
+                <strong>Debit balance available</strong>
+              </p>
+              <span>{`$${this.state.debitBalance}`}</span>
+            </div>
+          </div>
+          <div className={styles.send}>
+            <button className={styles.button}>Send to wallet</button>
+            <button className={styles.button}>Send to bank</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- pages/employer/manageStreams.js
+++ pages/employer/manageStreams.js
@@ -1,56 +1,81 @@
-import React from "react";
-import Image from "next/image";
-import styles from "../../styles/manageStreams.module.css"
-import SmallLogo from "../../components/smallLogo";
+import React from 'react';
+import Image from 'next/image';
+import styles from '../../styles/manageStreams.module.css';
+import SmallLogo from '../../components/smallLogo';
+import editBtn from '../../public/edit-button.svg';
+import deleteBtn from '../../public/delete-button.svg';
 
 export default class ManageStreams extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { company: "My company", lastBankSync: "July 27, 2022" };
-        this.handleUpload = this.handleUpload.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { company: 'My company', lastBankSync: 'July 27, 2022' };
+    this.handleUpload = this.handleUpload.bind(this);
+  }
 
-    handleUpload() {
-        console.log("handleUpload");
-    }
+  handleUpload() {
+    console.log('handleUpload');
+  }
 
-    render() {
-        return (
-            <div className={styles.container}>
-                <div className={styles.leftPanel}>
-                    <SmallLogo />
-                    <p>{this.state.company}</p>
-                    <button className={styles.button}>Sync bank account</button>
-                    <p>Last synced: {this.state.lastBankSync}</p>
-                </div>
-                <div className={styles.rightPanel}>
-                    <div className={styles.uploadFileBar}>
-                        <span>Upload a file with your employee salary information: </span>
-                        <button className={styles.button} onClick={this.handleUpload}>Upload</button>
-                    </div>
-                    <div>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th>Company</th>
-                                    <th>Contact</th>
-                                    <th>Country</th>
-                                </tr>
-                                <tr>
-                                    <td>Alfreds Futterkiste</td>
-                                    <td>Maria Anders</td>
-                                    <td>Germany</td>
-                                </tr>
-                                <tr>
-                                    <td>Alfreds Futterkiste</td>
-                                    <td>Maria Anders</td>
-                                    <td>Germany</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.leftPanel}>
+          <SmallLogo />
+          <p className={styles.company}>{this.state.company}</p>
+          <button className={styles.bankbutton}>Sync bank account</button>
+          <p className={styles.sync}>Last synced: {this.state.lastBankSync}</p>
+        </div>
+        <div className={styles.rightPanel}>
+          <div className={styles.uploadFileBar}>
+            <span>Upload a file with your employee salary information: </span>
+            <button className={styles.button} onClick={this.handleUpload}>
+              Upload
+            </button>
+          </div>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+                <th>Earnings</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>alfreds.f@company.xyz</td>
+                <td>Germany</td>
+                <td>3,485.04 €</td>
+                <td
+                  style={{ backgroundImage: `url(${editBtn.src})` }}
+                  className={styles.action}
+                ></td>
+                <td
+                  style={{ backgroundImage: `url(${deleteBtn.src})` }}
+                  className={styles.action}
+                ></td>
+              </tr>
+              <tr>
+                <td>Maria Anders</td>
+                <td>maria.a@company.xyz</td>
+                <td>USA</td>
+                <td>1,532.95 $</td>
+                <td
+                  style={{ backgroundImage: `url(${editBtn.src})` }}
+                  className={styles.action}
+                ></td>
+                <td
+                  style={{ backgroundImage: `url(${deleteBtn.src})` }}
+                  className={styles.action}
+                ></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
